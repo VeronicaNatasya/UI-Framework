7aa2a1e7877155b03ff2e67f63332deb37b046e1
VeronicaNatasya <veronicanatasya35@gmail.com>
2022-04-18 14:33:23 +0700

    Add Task

---
+++ Praktek/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  
+</script>
+
+<style scoped>
+  .newsItem{
+    color: white;
+    text-decoration: none;
+  }
+
+</style>
